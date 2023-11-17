dd96758b7492f7ca528ebae8b2f6ba6787e3204f
wdy <wangdy@lglab.ac.cn>
2023-11-17 16:58:11 +0800

    202311171658

--- wcode/mol/fingerprint.py
+++ wcode/mol/fingerprint.py
@@ -1,13 +1,35 @@
 from rdkit import Chem
-from rdkit import DataStructs
 from rdkit.Chem import AllChem
-import pandas as pd
-from sklearn.cluster import KMeans
+from rdkit.Chem import MACCSkeys
+import numpy as np
 
 ########################################################################################################################
 
-def morgan_fingerprint(smiles):
-    mol = Chem.MolFromSmiles(smiles)
-    return AllChem.GetMorganFingerprintAsBitVect(mol, 2, 1024)
+def smiles2fp(smiles,
+              fp_type=None,
+              as_np=True):
+    """
+
+    :param smiles:
+    :param fp_type:
+    :return:
+    """
+    if fp_type is None:
+        fp_type = ['morgan']
+    fps = {fpt: [] for fpt in fp_type}
+    for i in range(len(smiles)):
+        rdkit_mol = Chem.MolFromSmiles(smiles[i])
+
+        for fpt in fp_type:
+            if fpt == 'morgan':
+                fp = AllChem.GetMorganFingerprintAsBitVect(rdkit_mol, 2, 1024)
+            elif fpt == 'maccs':
+                fp = MACCSkeys.GenMACCSKeys(rdkit_mol)
+
+            fps[fpt].append(fp)
+    if as_np:
+        return [np.array(fps[fpt], dtype=np.int64) for fpt in fp_type]
+    else:
+        return [fps[fpt] for fpt in fp_type]
 
 ########################################################################################################################
---
+++ wcode/protein/uniprot.py
@@ -0,0 +1,20 @@
+import requests as r
+from Bio import SeqIO
+from io import StringIO
+
+########################################################################################################################
+
+
+cID='P04637'
+def get_uniprot_sequence(uniprot_id):
+    baseUrl="http://www.uniprot.org/uniprot/"
+    currentUrl=baseUrl+cID+".fasta"
+    response = r.post(currentUrl)
+    cData=''.join(response.text)
+
+    Seq=StringIO(cData)
+    pSeq=list(SeqIO.parse(Seq,'fasta'))
+    return pSeq
+
+########################################################################################################################
+

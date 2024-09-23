5900899043b4e8da6cca23ee14d0b11689d52bc2
wangdingyan <wangdy@lglab.ac.cn>
2024-09-23 16:57:01 +0800

    20240829

---
+++ pipeline/tmp/database/2_sifts.pl
@@ -0,0 +1,149 @@
+#!/usr/bin/perl
+# download uniprot accession, taxonomy, EC, GO and pubmed from sifts
+# download ncbi taxonomy and scientific name from ncbi
+use strict;
+use File::Basename;
+use Cwd 'abs_path';
+my $rootdir = "/mnt/d/WDrugDataset";
+
+print "download go\n";
+system("mkdir -p $rootdir/obo/go/") if (!-d "$rootdir/obo/go/");
+system("wget -q http://purl.obolibrary.org/obo/go/go-basic.obo -O $rootdir/obo/go/go-basic.obo");
+system("wget -q http://current.geneontology.org/ontology/go-basic.obo -O $rootdir/obo/go/go-basic.obo") if (!-s "$rootdir/obo/go/go-basic.obo");
+
+print "download swissprot\n";
+system("mkdir -p $rootdir/uniprot/current_release/knowledgebase/complete") if (!-d "$rootdir/uniprot/current_release/knowledgebase/complete");
+system("wget -q http://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz -O $rootdir/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz");
+system("wget -q ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz -O $rootdir/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz") if (!-s "$rootdir/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz");
+
+print "download sifts\n";
+system("mkdir -p $rootdir/sifts/flatfiles/tsv");
+foreach my $filename(("pdb_chain_uniprot.tsv.gz",
+                      "pdb_chain_taxonomy.tsv.gz",
+                      "pdb_pubmed.tsv.gz",
+                      "pdb_chain_enzyme.tsv.gz",
+                      "pdb_chain_go.tsv.gz"))
+{
+    system("wget -q http://ftp.ebi.ac.uk/pub/databases/msd/sifts/flatfiles/tsv/$filename -O $rootdir/sifts/flatfiles/tsv/$filename");
+    system("wget -q ftp://ftp.ebi.ac.uk/pub/databases/msd/sifts/flatfiles/tsv/$filename -O $rootdir/sifts/flatfiles/tsv/$filename") if (!-s "$rootdir/sifts/flatfiles/tsv/$filename");
+}
+
+system("mkdir -p $rootdir/taxonomy");
+system("wget -q http://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz -O $rootdir/taxonomy/taxdump.tar.gz");
+system("wget -q  ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz -O $rootdir/taxonomy/taxdump.tar.gz") if (!-s "$rootdir/taxonomy/taxdump.tar.gz");
+system("cd $rootdir/taxonomy; tar -xvf taxdump.tar.gz names.dmp");
+if (-s "$rootdir/taxonomy/names.dmp")
+{
+    system("rm $rootdir/taxonomy/taxdump.tar.gz");
+    &gzipFile("$rootdir/taxonomy/names.dmp");
+}
+
+system("mkdir -p $rootdir/data");
+print "parsing taxonomy\n";
+if (-s "$rootdir/sifts/flatfiles/tsv/pdb_chain_taxonomy.tsv.gz")
+{
+    system("zcat $rootdir/sifts/flatfiles/tsv/pdb_chain_taxonomy.tsv.gz |tail -n +3|cut -f1-3|uniq > $rootdir/data/chain2taxonomy.tsv");
+}
+my @taxid_list;
+foreach my $taxid(`cat $rootdir/data/chain2taxonomy.tsv|cut -f3|sort|uniq`)
+{
+    chomp($taxid);
+    push(@taxid_list,($taxid));
+}
+
+my %taxid_dict = map { $_ => "" } @taxid_list;
+foreach my $line(`zcat $rootdir/taxonomy/names.dmp.gz`)
+{
+    my @items=split(/\t\|\t/,$line);
+    my $taxid=$items[0];
+    next if (!exists $taxid_dict{$taxid});
+    my $name_type=$items[-1];
+    next if (length $taxid_dict{$taxid} && $name_type!~/scientific name/);
+    $taxid_dict{$taxid}=$items[1];
+}
+my $txt="";
+foreach my $taxid(@taxid_list)
+{
+    $txt.="$taxid\t$taxid_dict{$taxid}\n";
+}
+if (scalar @taxid_list)
+{
+    open(FP,">$rootdir/data/taxid2name.tsv");
+    print FP $txt;
+    close(FP);
+    &gzipFile("$rootdir/data/taxid2name.tsv");
+}
+&concatenate_sift("cat $rootdir/data/chain2taxonomy.tsv","$rootdir/data/chain2taxonomy.tsv");
+
+print "parsing uniprot accession\n";
+if (-s "$rootdir/sifts/flatfiles/tsv/pdb_chain_uniprot.tsv.gz")
+{
+    &concatenate_sift("zcat $rootdir/sifts/flatfiles/tsv/pdb_chain_uniprot.tsv.gz |tail -n +3|cut -f1-3|sort|uniq","$rootdir/data/chain2uniprot.tsv");
+}
+
+print "parsing EC\n";
+if (-s "$rootdir/sifts/flatfiles/tsv/pdb_chain_enzyme.tsv.gz")
+{
+    &concatenate_sift("zcat $rootdir/sifts/flatfiles/tsv/pdb_chain_enzyme.tsv.gz |tail -n +3|cut -f1,2,4|sort|uniq","$rootdir/data/chain2ec.tsv");
+}
+
+print "parsing GO\n";
+if (-s "$rootdir/sifts/flatfiles/tsv/pdb_chain_go.tsv.gz")
+{
+    &concatenate_sift("zcat $rootdir/sifts/flatfiles/tsv/pdb_chain_go.tsv.gz |tail -n +3|cut -f1,2,6|sort|uniq","$rootdir/data/chain2go.tsv");
+}
+
+print "parsing pubmed\n";
+if (-s "$rootdir/sifts/flatfiles/tsv/pdb_pubmed.tsv.gz")
+{
+    system("zcat $rootdir/sifts/flatfiles/tsv/pdb_pubmed.tsv.gz |tail -n +3|grep -P '\\t0\\t'|cut -f1,3|sort|uniq > $rootdir/data/pdb2pubmed.tsv");
+    &gzipFile("$rootdir/data/pdb2pubmed.tsv");
+}
+
+exit(0);
+
+sub gzipFile
+{
+    my ($filename)=@_;
+    my $oldNum=`zcat $filename.gz 2>/dev/null|wc -l`+0;
+    my $newNum=` cat $filename   |wc -l`+0;
+    if (0.8*$oldNum>$newNum)
+    {
+        print "WARNING! do not update $filename from $oldNum to $newNum entries\n";
+        return;
+    }
+    print "update $filename from $oldNum to $newNum entries\n";
+    system("gzip -f $filename");
+    return;
+}
+
+sub concatenate_sift
+{
+    my ($cmd,$outfile)=@_;
+    my @chain_list;
+    my %chain_dict;
+    foreach my $line(`$cmd`)
+    {
+        chomp($line);
+        my @items=split("\t",$line);
+        my $chain="$items[0]\t$items[1]";
+        if (exists $chain_dict{$chain})
+        {
+            $chain_dict{$chain}.=",$items[2]";
+        }
+        else
+        {
+            push(@chain_list,($chain));
+            $chain_dict{$chain}="$items[2]";
+        }
+    }
+    my $txt="";
+    foreach my $chain(@chain_list)
+    {
+        $txt.="$chain\t$chain_dict{$chain}\n";
+    }
+    open(FP,">$outfile");
+    print FP $txt;
+    close(FP);
+    &gzipFile($outfile);
+}
---
+++ pipeline/tmp/database/3_bindingdb.pl
@@ -0,0 +1,145 @@
+#!/usr/bin/perl
+# download binding affinity from BindingDB and PDBbind-CN
+use strict;
+use File::Basename;
+use Cwd 'abs_path';
+my $rootdir = "/mnt/d/WDrugDataset";
+
+print "download BindingDB\n";
+system("mkdir -p $rootdir/bind");
+system("wget -q --no-check-certificate 'https://www.bindingdb.org/rwd/bind/chemsearch/marvin/Download.jsp' -O $rootdir/bind/Download.jsp");
+if (`cat $rootdir/bind/Download.jsp`=~/(BindingDB_All_\w+.tsv.zip)/)
+{
+    my $outfile="$1";
+    system("wget -q --no-check-certificate 'https://www.bindingdb.org/rwd/bind/downloads/$outfile' -O $rootdir/bind/BindingDB_All_tsv.zip");
+    if (-s "$rootdir/bind/BindingDB_All_tsv.zip")
+    {
+        system("zcat $rootdir/bind/BindingDB_All_tsv.zip| cut -f9-14,27,28,42,43,47|  head -1 > $rootdir/bind/BindingDB.tsv");
+        system("zcat $rootdir/bind/BindingDB_All_tsv.zip| cut -f9-14,27,28,42,43,47|grep -P '\\S+\\t\\S+\\t\\S*\\t\\S*\\t\\S*\$' |grep -vP '\\t\\t\\t\$'  >> $rootdir/bind/BindingDB.tsv");
+        &gzipFile("$rootdir/bind/BindingDB.tsv");
+    }
+}
+
+my %pdb2chain;
+my %chain2uniprot;
+foreach my $line(`zcat $rootdir/data/chain2uniprot.tsv.gz`)
+{
+    if ($line=~/^(\S+)\t(\S+)\t(\S+)$/)
+    {
+        my $pdbid  ="$1";
+        my $asym_id="$2";
+        my $uniprot="$3";
+        if (!exists $pdb2chain{$pdbid}) { $pdb2chain{$pdbid}="$asym_id"; }
+        else                          { $pdb2chain{$pdbid}.=",$asym_id"; }
+        $chain2uniprot{"$pdbid\t$asym_id"}=$uniprot;
+    }
+}
+
+#  0 Ki (nM)
+#  1 IC50 (nM)
+#  2 Kd (nM)
+#  3 EC50 (nM)
+#  4 kon (M-1-s-1)
+#  5 koff (s-1)
+#  6 Ligand HET ID in PDB
+#  7 PDB ID(s) for Ligand-Target Complex
+#  8 UniProt (SwissProt) Primary ID of Target Chain
+#  9 UniProt (SwissProt) Secondary ID(s) of Target Chain
+# 10 UniProt (TrEMBL) Primary ID of Target Chain
+my @header_list=split(/\t/,`zcat $rootdir/bind/BindingDB.tsv.gz|head -1`);
+my @BindingDB_list;
+my %BindingDB_dict;
+foreach my $line(`zcat $rootdir/bind/BindingDB.tsv.gz|tail -n +2`)
+{
+    chomp($line);
+    my @items=split(/\t/,$line);
+    my $uniprot_line="$items[8],$items[9],$items[10]";
+
+    my $affinity="";
+    for (my $i=0;$i<6;$i++)
+    {
+        if ($items[$i]=~/(\S+)/)
+        {
+            my $kd="$1";
+            my $first=$header_list[$i];
+            my $second="";
+            if ($first=~/(\S+)\s\((\S+)\)/)
+            {
+                $first="$1";
+                $second="$2";
+            }
+            if (length $affinity) { $affinity.=", $first=$kd$second"; }
+            else                  { $affinity =  "$first=$kd$second"; }
+        }
+    }
+    next if (length $affinity==0);
+
+    my $ccd     =   $items[6];
+    my $pdb_line=lc($items[7]);
+    foreach my $pdbid(split(/,/,$pdb_line))
+    {
+        foreach my $asym_id(split(/,/,$pdb2chain{$pdbid}))
+        {
+            my $uniprot=$chain2uniprot{"$pdbid\t$asym_id"};
+            if ($uniprot_line=~/\b$uniprot\b/)
+            {
+                my $key="$pdbid\t$asym_id\t$ccd";
+                if (!exists($BindingDB_dict{$key}))
+                {
+                    push(@BindingDB_list,($key));
+                    $BindingDB_dict{$key}=$affinity;
+                }
+                else
+                {
+                    $BindingDB_dict{$key}.=", $affinity";
+                }
+            }
+        }
+    }
+}
+my $txt="";
+foreach my $key(@BindingDB_list)
+{
+    my $value=$BindingDB_dict{$key};
+    my %exist_kd;
+    my $uniq_value="";
+    foreach my $kd(split(/, /,$value))
+    {
+        if (exists $exist_kd{$kd}) { $exist_kd{$kd}+=1; }
+        else                       { $exist_kd{$kd}=1;  }
+    }
+    my %exist_key;
+    foreach my $kd (sort {$exist_kd{$b} <=> $exist_kd{$a}} keys %exist_kd)
+    {
+        if ($kd=~/^(\S+)=/)
+        {
+            my $key="$1";
+            next if (exists $exist_key{$key});
+            $exist_key{$key}=1;
+            if (length $uniq_value==0) { $uniq_value=$kd; }
+            else { $uniq_value.=", $kd"; }
+        }
+    }
+    $txt.="$key\t$uniq_value\n";
+}
+open(FP,">$rootdir/data/BindingDB.tsv");
+print FP "$txt";
+close(FP);
+&gzipFile("$rootdir/data/BindingDB.tsv");
+
+exit();
+
+sub gzipFile
+{
+    my ($filename)=@_;
+    my $oldNum=`zcat $filename.gz 2>/dev/null|wc -l`+0;
+    my $newNum=` cat $filename   |wc -l`+0;
+    if (0.8*$oldNum>$newNum)
+    {
+        print "WARNING! do not update $filename from $oldNum to $newNum entries\n";
+        return;
+    }
+    print "update $filename from $oldNum to $newNum entries\n";
+    system("gzip -f $filename");
+    return;
+}
